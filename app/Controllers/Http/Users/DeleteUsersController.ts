33e1745f90356f08fa3046873894d90fb13a35b8
Dahan Schuster <tanbohe@duck.com>
2024-04-12 10:26:59 -0300

    fix: remoção de token ao inativar usuário

--- app/Controllers/Http/Users/DeleteUsersController.ts
+++ app/Controllers/Http/Users/DeleteUsersController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Database from "@ioc:Adonis/Lucid/Database";
 import User from "App/Models/User";
 import { DateTime } from "luxon";
 
@@ -9,7 +10,7 @@ export default class DeleteUsersController {
     const id = request.param("id", 0);
     const user = await User.findOrFail(id);
 
-    await auth.use("jwt").revoke();
+    await Database.from("jwt_tokens").where("user_id", id).delete();
     await user
       .merge({ status: 0, deletedBy: loggedUser.id, deletedAt: DateTime.now() })
       .save();
