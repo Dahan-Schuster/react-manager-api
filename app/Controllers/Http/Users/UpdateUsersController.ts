7d8ba24d29b2994394af69bda50c35c7f339c9d1
Dahan Schuster <tanbohe@duck.com>
2024-04-12 10:26:44 -0300

    fix: move id para parâmetro da rota no UpdateUsers

--- app/Controllers/Http/Users/UpdateUsersController.ts
+++ app/Controllers/Http/Users/UpdateUsersController.ts
@@ -6,16 +6,16 @@ import User from "App/Models/User";
 export default class UpdateUsersController {
   public async handle({ request, response }: HttpContextContract) {
     const newUserSchema = schema.create({
-      id: schema.number(),
       nome: schema.string.optional({ trim: true }),
       email: schema.string.optional([rules.email()]),
     });
 
-    const { id, nome, email } = await request.validate({
+    const { nome, email } = await request.validate({
       schema: newUserSchema,
       messages: userValidationMessages,
     });
 
+    const id = request.param("id", 0);
     const user = await User.findOrFail(id);
     await user.merge({ nome, email }).save();
 
