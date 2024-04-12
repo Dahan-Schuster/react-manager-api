0ca98fbb86696b0657ede5fe5a2aea722fae281e
Dahan Schuster <tanbohe@duck.com>
2024-04-12 17:55:19 -0300

    feat: permissões das rotas CRUD de usuário

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -1,16 +1,22 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.get("/", "Users/GetUsersController");
-  Route.get("/:id", "Users/ShowUserController");
-  Route.post("/", "Users/StoreUsersController");
-  Route.put("/:id", "Users/UpdateUsersController");
-  Route.delete("/:id", "Users/DeleteUsersController");
+  Route.get("/", "Users/GetUsersController").middleware("auth:usuarios-listar");
+  Route.get("/:id", "Users/ShowUserController").middleware(
+    "auth:usuarios-listar"
+  );
+  Route.post("/", "Users/StoreUsersController").middleware(
+    "auth:usuarios-criar"
+  );
+  Route.put("/:id", "Users/UpdateUsersController").middleware(
+    "auth:usuarios-editar"
+  );
+  Route.delete("/:id", "Users/DeleteUsersController").middleware(
+    "auth:usuarios-deletar"
+  );
 
   Route.group(() => {
     Route.post("", "Users/LinkPermissaoUsuarioController");
     Route.delete("", "Users/UnlinkPermissaoUsuarioController");
   }).prefix("/:id/permissoes/:idPermissao");
-})
-  .prefix("usuario")
-  .middleware("auth");
+}).prefix("usuario");
