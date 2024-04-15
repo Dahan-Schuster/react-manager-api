f1cf002d02cb2dc0135c5d0068fc9e173316b251
Dahan Schuster <tanbohe@duck.com>
2024-04-15 12:38:18 -0300

    feat: seeder para permissões do user admin@teste.com.br

---
+++ database/seeders/PermissoesAdmin.ts
@@ -0,0 +1,36 @@
+import { ModelObject } from "@ioc:Adonis/Lucid/Orm";
+import BaseSeeder from "@ioc:Adonis/Lucid/Seeder";
+import Permissao from "App/Models/Permissao";
+import User from "App/Models/User";
+
+export default class extends BaseSeeder {
+  public async run() {
+    const user = await User.query()
+      .where("email", "admin@teste.com.br")
+      .first();
+
+    if (!user) return;
+
+    const permissoes = await Permissao.query()
+      .select("id")
+      .whereIn("modulo_id", [1, 2]);
+
+    await user.load("permissoes");
+    const idsPermissoesUser = user.permissoes.map((p) => p.id);
+    const idsNovasPermissoes = permissoes
+      .filter((p) => !idsPermissoesUser.includes(p.id))
+      .map((p) => p.id);
+
+    console.log(
+      "Novas permissões para o admin:",
+      idsNovasPermissoes.toString()
+    );
+
+    await user.related("permissoes").attach(
+      idsNovasPermissoes.reduce((prev, curr) => {
+        prev[curr] = { permissao_fixada: true };
+        return prev;
+      }, {} as Record<string, ModelObject>)
+    );
+  }
+}
