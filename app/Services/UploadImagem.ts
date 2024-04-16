28bd818550a864ab09dafef44f20aa3a3054ed08
Dahan Schuster <tanbohe@duck.com>
2024-04-16 17:39:46 -0300

    feat: configuração do Drive e service UploadImagem

---
+++ app/Services/UploadImagem.ts
@@ -0,0 +1,34 @@
+import type { MultipartFileContract } from "@ioc:Adonis/Core/BodyParser";
+import type { ContentHeaders } from "@ioc:Adonis/Core/Drive";
+import Drive from "@ioc:Adonis/Core/Drive";
+import randomstring from "randomstring";
+import StringTools from "./StringTools";
+
+export default class UploadImagem {
+  public static async upload(
+    file: MultipartFileContract,
+    opcoes: {
+      visibility?: string | undefined;
+    } & ContentHeaders &
+      Record<string, any> & {
+        name?: string | undefined;
+      } = {},
+    pasta = ""
+  ) {
+    const fileName = opcoes.name || file.clientName;
+    opcoes.name = `${randomstring.generate(10)}_${StringTools.slugify(
+      fileName.split(".")[0]
+    )}.${file.extname}`;
+
+    if (!opcoes.visibility) {
+      opcoes.visibility = "public";
+    }
+
+    await file.moveToDisk(pasta, opcoes);
+    return Drive.getUrl(pasta + "/" + opcoes.name);
+  }
+
+  public static async delete(filePath: string) {
+    await Drive.delete(filePath);
+  }
+}
