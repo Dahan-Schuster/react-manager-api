67bd44436e9b6e3558caf3b0cb9f41a4d7b148a8
Dahan Schuster <tanbohe@duck.com>
2024-04-24 13:59:25 -0300

    fix: não retorna erro se não houver tema ativo

--- app/Controllers/Http/Temas/Mui/GetTemaAtivoController.ts
+++ app/Controllers/Http/Temas/Mui/GetTemaAtivoController.ts
@@ -1,11 +1,9 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import ApiError from "App/Exceptions/ApiError";
 import TemaMuiSistema from "App/Models/TemaMuiSistema";
 
 export default class GetTemaAtivoController {
   public async getTema({ response }: HttpContextContract, mode: MUI.MuiMode) {
     const tema = await TemaMuiSistema.getAtivo(mode);
-    if (!tema) throw new ApiError("Nenhum tema encontrado", 404);
 
     return response.send({
       success: true,
