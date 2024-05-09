04041554a03e162dfd6339449a3d5bded1a3f219
Dahan <tanbohe@duck.com>
2024-05-09 16:34:40 -0300

    fix: remove controller de edição de módulos
    - Para evitar problemas com a configuração das permissões de cada
      módulo, remove o controller que edita essas permissões
    
    - A edição dos tipos de permissão de cada módulo deve ser feita no
      seeder PermissoesModulos

--- start/routes/modulos.ts
+++ start/routes/modulos.ts
@@ -1,10 +1,5 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.group(() => {
-    // rota para alterar os tipos de permissão disponíveis para o módulo
-    Route.put("/", "Modulos/UpdateTiposPermissoesModuloController");
-  }).prefix(":id/tiposPermissoes");
-
   Route.get("/", "Modulos/GetModulosController");
 }).prefix("modulo");
