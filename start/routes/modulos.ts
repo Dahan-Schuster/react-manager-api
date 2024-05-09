cb3bce5c2c4bf1bd6ba9c3b3db4ba277e6355cd0
Dahan <tanbohe@duck.com>
2024-05-09 17:36:31 -0300

    feat: permissões da rota GET modulos

--- start/routes/modulos.ts
+++ start/routes/modulos.ts
@@ -1,5 +1,7 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.group(() => {
-  Route.get("/", "Modulos/GetModulosController");
+  Route.get("/", "Modulos/GetModulosController").middleware(
+    "auth:perfis-listar,perfis-editar,usuarios-editar,usuarios-criar"
+  );
 }).prefix("modulo");
