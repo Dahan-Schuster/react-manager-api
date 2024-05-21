a7b8847f481af4824ee1497cd12c2b6fbbb39e6b
Dahan <tanbohe@duck.com>
2024-05-21 16:10:31 -0300

    feat: paginação de logs

--- app/Controllers/Http/DatabaseLogs/GetDatabaseLogsController.ts
+++ app/Controllers/Http/DatabaseLogs/GetDatabaseLogsController.ts
@@ -1,9 +1,14 @@
+import Env from "@ioc:Adonis/Core/Env";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import DatabaseLog from "App/Models/DatabaseLog";
 
 export default class GetDatabaseLogsController {
-  public async handle({ response }: HttpContextContract) {
-    const logs = await DatabaseLog.query();
+  public async handle({ request, response }: HttpContextContract) {
+    const page = request.input("page", 1);
+    const limit = request.input("per_page", Env.get("DEFAULT_PER_PAGE"));
+
+    const logs = await DatabaseLog.query().orderBy("created_at", "desc").paginate(page, limit);
+
     response.send({
       success: true,
       logs,
